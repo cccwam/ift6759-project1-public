a83eff7c38ad51277e61cbd1913ad92e3d7c5e8b
ilyasamaniss <ilyas.amaniss@mail.mcgill.ca>
2020-02-01 20:02:34 -0500

    Starting preprocessing datacleaning steps

--- train.py
+++ train.py
@@ -2,10 +2,17 @@
 #   Trains the predictor
 
 import tensorflow as tf
-
+import panda as pd
 
 def main():
     # TODO: Implement
+
+    # Read data
+    data_path = Path(r"/project/cq-training-1/project1/data/")
+    catalog = pd.read_pickle(data_path/"catalog.helios.public.20100101-20160101.pkl")
+
+    # Clean data: remove night values and take care of Nan values
+    cleanData(catalog)
 
     tf.debugging.set_log_device_placement(True)
 
@@ -20,6 +27,28 @@ def main():
     c = tf.matmul(a, b)
     print(c)
 
+
+def cleanData(dataframe):
+    newDataSet = removeNightValues(dataframe)
+    newDataSet = removeNanValues(newDataSet)
+    newDataSet = fillGHIValues(newDataSet)
+
+
+def removeNightValues(dataframe):
+    return dataframe[(dataframe.DRA_DAYTIME==1) |
+                            (dataframe.TBL_DAYTIME==1) |
+                            (dataframe.BND_DAYTIME==1) |
+                            (dataframe.FPK_DAYTIME==1) | 
+                            (dataframe.GWN_DAYTIME==1) | 
+                            (dataframe.PSU_DAYTIME==1) |
+                            (dataframe.SXF_DAYTIME==1)]
+
+def removeNanValues(dataframe):
+    pass
+
+
+def fillGHIValues(dataframe):
+    pass
 
 if __name__ == '__main__':
     main()

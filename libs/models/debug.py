5f3bf38e28aad3fa97cd6fb149c90f08f140a936
Blaise Gauvin St-Denis <bstdenis@gmail.com>
2020-02-11 19:01:45 -0500

    Add simple MLP test

--- libs/models/debug.py
+++ libs/models/debug.py
@@ -49,3 +49,38 @@ def test_dataloader(
     model = TestDataLoader(target_time_offsets)
 
     return model
+
+
+def test_mlp(
+        stations: typing.Dict[typing.AnyStr, typing.Tuple[float, float, float]],
+        target_time_offsets: typing.List[datetime.timedelta],
+        config: typing.Dict[typing.AnyStr, typing.Any],
+):
+    """This function should be modified in order to prepare & return your own prediction model.
+
+    Args:
+        stations: a map of station names of interest paired with their coordinates (latitude, longitude, elevation).
+        target_time_offsets: the list of timedeltas to predict GHIs for (by definition: [T=0, T+1h, T+3h, T+6h]).
+        config: configuration dictionary holding any extra parameters that might be required by the user. These
+            parameters are loaded automatically if the user provided a JSON file in their submission. Submitting
+            such a JSON file is completely optional, and this argument can be ignored if not needed.
+
+    Returns:
+        A model
+    """
+
+    class TestMLP(tf.keras.Model):
+
+        def __init__(self, target_time_offsets):
+            super(TestMLP, self).__init__()
+            self.flatten = tf.keras.layers.Flatten()
+            self.dense1 = tf.keras.layers.Dense(128, activation=tf.nn.relu)
+            self.dense2 = tf.keras.layers.Dense(len(target_time_offsets))
+
+        def call(self, inputs):
+            x = self.dense1(self.flatten(inputs))
+            return self.dense2(x)
+
+    model = TestMLP(target_time_offsets)
+
+    return model

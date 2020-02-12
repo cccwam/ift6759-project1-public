ee1ac7c68bf0f20264dc5c0150306ea153301300
ilyasamaniss <ilyas.amaniss@mail.mcgill.ca>
2020-02-12 17:19:23 -0500

    CircleCi attempt2

--- libs/data_loaders/datacleaning.py
+++ libs/data_loaders/datacleaning.py
@@ -3,15 +3,17 @@
 def removeNightValues(dataframe):
 
     return dataframe[(dataframe.DRA_DAYTIME == 1) |
-                    (dataframe.TBL_DAYTIME == 1) |
-                    (dataframe.BND_DAYTIME == 1) |
-                    (dataframe.FPK_DAYTIME == 1) | 
-                    (dataframe.GWN_DAYTIME == 1) | 
-                    (dataframe.PSU_DAYTIME == 1) |
-                    (dataframe.SXF_DAYTIME == 1)]
+                     (dataframe.TBL_DAYTIME == 1) |
+                     (dataframe.BND_DAYTIME == 1) |
+                     (dataframe.FPK_DAYTIME == 1) |
+                     (dataframe.GWN_DAYTIME == 1) |
+                     (dataframe.PSU_DAYTIME == 1) |
+                     (dataframe.SXF_DAYTIME == 1)]
+
 
 def removeNullPath(dataframe):
-    return dataframe[dataframe['ncdf_path']!='nan']
+    return dataframe[dataframe['ncdf_path'] != 'nan']
+
 
 # Since the first GHI values of the DRA station are NaN, it cannot
 # inteprolate values, we will have to decide how to take of them

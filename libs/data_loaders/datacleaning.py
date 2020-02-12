fc8d7b2a91f1db4e1e23548cced8492e19c691c9
ilyasamaniss <ilyas.amaniss@mail.mcgill.ca>
2020-02-12 17:12:58 -0500

    solve circleCI demands

--- libs/data_loaders/datacleaning.py
+++ libs/data_loaders/datacleaning.py
@@ -1,14 +1,14 @@
-import pandas as pd
+
 
 def removeNightValues(dataframe):
 
-    return dataframe[(dataframe.DRA_DAYTIME==1) |
-                    (dataframe.TBL_DAYTIME==1) |
-                    (dataframe.BND_DAYTIME==1) |
-                    (dataframe.FPK_DAYTIME==1) | 
-                    (dataframe.GWN_DAYTIME==1) | 
-                    (dataframe.PSU_DAYTIME==1) |
-                    (dataframe.SXF_DAYTIME==1)]
+    return dataframe[(dataframe.DRA_DAYTIME == 1) |
+                    (dataframe.TBL_DAYTIME == 1) |
+                    (dataframe.BND_DAYTIME == 1) |
+                    (dataframe.FPK_DAYTIME == 1) | 
+                    (dataframe.GWN_DAYTIME == 1) | 
+                    (dataframe.PSU_DAYTIME == 1) |
+                    (dataframe.SXF_DAYTIME == 1)]
 
 def removeNullPath(dataframe):
     return dataframe[dataframe['ncdf_path']!='nan']
@@ -20,5 +20,5 @@ def fillGHI(dataframe):
     stations = ['BND', 'TBL', 'DRA', 'FPK', 'GWN', 'PSU', 'SXF']
     for station in stations:
         dataframe[f'{station}_GHI'] = (dataframe[f"{station}_GHI"]).interpolate(method='linear')
-    
+
     return dataframe

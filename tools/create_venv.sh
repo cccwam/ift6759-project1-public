7cb778a9c3eb552ddadd93b993f3d9ae8acad055
Helgi Tomas Gislason <helgso@gmail.com>
2020-02-13 20:47:53 -0500

    Only looking in the generic folder for python packages. Otherwise installation doesn't always work. Also fixing a bug where a partial package name was still being matched.

--- tools/create_venv.sh
+++ tools/create_venv.sh
@@ -1,5 +1,7 @@
+set -e
+
 # Summary:
-#   1. Creates a virtual environment in the current working directory.
+#   1. Creates a virtual environment in LOCAL_VENV_FOLDER.
 #   2. Installs dependencies from $LOCAL_GIT_REPO_FOLDER/requirements.txt
 #   3. Makes sure helios compiled packages are installed when available.
 #      When not avaiable, online pypi packages will be used.
@@ -8,32 +10,51 @@
 #   2. Clone this git repository to your helios home folder.
 #   3. Edit LOCAL_GIT_REPO_FOLDER and LOCAL_VENV_FOLDER as necessary.
 # Example usage:
-#   ~/ift6759-project1/tools/create_venv.sh
+#   /create_venv.sh
 
 LOCAL_GIT_REPO_FOLDER=~/ift6759-project1
-VENV_FOLDER_TO_CREATE=ift6759-project1-venv
+LOCAL_VENV_FOLDER=~/ift6759-project1-venv
 
+if [ -d "$LOCAL_VENV_FOLDER" ];
+then
+  echo "Error: The folder $LOCAL_VENV_FOLDER already exists. Exiting."
+  exit 1
+fi
+
+module load hdf5/1.10.3
 module load python/3.7.4
-virtualenv --no-download $VENV_FOLDER_TO_CREATE
-source $VENV_FOLDER_TO_CREATE/bin/activate
+virtualenv --no-download $LOCAL_VENV_FOLDER
+source $LOCAL_VENV_FOLDER/bin/activate
 
-avail_helios_packages=$(ls -lR /cvmfs/soft.computecanada.ca/custom/python/wheelhouse/)
+avail_helios_packages=$(ls /cvmfs/soft.computecanada.ca/custom/python/wheelhouse/generic)
 required_packages=$(sed -e 's/#.*$//' -e '/^$/d' $LOCAL_GIT_REPO_FOLDER/requirements.txt)
+if [[ "$required_packages" == *+(<|>)* ]]
+then
+  echo "Error: One of the requirements uses < or >. This is not supported. Full list of requirements: $required_packages. Exiting."
+  exit 1
+fi
+
 online_packages=""
 offline_packages=""
 for requirement in $required_packages; do
-  helios_name=$(echo "$requirement" | sed -r 's/[=<>]+/-/g')
-  if [[ "$avail_helios_packages" == *"$helios_name"* ]]
+  requirement_split=( ${requirement//[=]/ } )
+  package_name="${requirement_split[0]}"
+  version="${requirement_split[1]}"
+  if [[ -z $version ]]
   then
-    offline_packages+="$requirement "
-  else
+    version="[0-9]+.[0-9]+.[0-9]+"
+  fi
+  if [[ -z $(echo $avail_helios_packages | egrep $package_name-$version) ]]
+  then
     online_packages+="$requirement "
+  else
+    offline_packages+="$requirement "
   fi
 done
 
 echo "Upgrading pip..."
 pip install --no-index --upgrade pip
-echo "Installing required packages available on helios: $offline_packages ..."
+echo "Installing required packages available on helios: $offline_packages..."
 pip install --no-index $offline_packages
-echo "Installing required packages not available on helios: $online_packages ..."
+echo "Installing required packages not available on helios: $online_packages..."
 pip install $online_packages

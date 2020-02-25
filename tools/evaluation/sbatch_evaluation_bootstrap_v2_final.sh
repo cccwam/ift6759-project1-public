a0693464ac9a8f47a201a4f52763ca9765dd0daa
Francois Mercier <cccwam@gmail.com>
2020-02-24 20:45:38 -0500

    Reformat JSON files to multiline

---
+++ tools/evaluation/sbatch_evaluation_bootstrap_v2_final.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:k20:1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=6G
+# SBATCH --reservation=IFT6759_2020-02-21
+
+# Summary:
+#   A template for launching a batch job to execute code from
+#   this repository on a k80 gpu node.
+# Pre-requisites:
+#   1. Ensure you have internet access (be on a login node).
+#   2. Clone this git repository to your helios home folder.
+#   3. Create a virtual environment in your home folder:
+#      ~/ift6759-project1/tools/create_venv.sh
+#   4. Copy the sbatch_template.sh to your home folder:
+#      cp ~/ift6759-project1/tools/sbatch_template.sh ~/
+#   5. In ~/sbatch_template.sh, perform necessary modifications
+#      to the SBATCH parameters at the top of this script and
+#      the python command at the bottom.
+#   6. Edit LOCAL_GIT_REPO_FOLDER and LOCAL_VENV_FOLDER as necessary.
+# Example usage:
+#   sbatch ~/sbatch_template.sh
+
+LOCAL_GIT_REPO_FOLDER=~/ift6759-project1
+LOCAL_VENV_FOLDER=~/py37_tf
+
+cd $SLURM_TMPDIR/
+cp -a $LOCAL_GIT_REPO_FOLDER/. $SLURM_TMPDIR/
+cp -a $LOCAL_VENV_FOLDER/. $SLURM_TMPDIR/venv/
+module load python/3.7.4
+module load hdf5-mpi/1.10.3
+source $SLURM_TMPDIR/venv/bin/activate
+
+
+for i in {0..29..1}
+    do
+      python evaluator.py \
+      ~/ift6759-project1/pred_output_test_${i}.txt \
+      configs/admin/hourly_sample_daytime_${i}_test.json \
+      --user_cfg_path configs/user/cnn_image_hourly_daytime_v2_pretrained.json \
+      --stats_output_path ~/ift6759-project1/stat_output_test_${i}.txt
+    done
